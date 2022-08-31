66755b09a69128c5bc436eea614d4922480e7680
Chigileychik Andrey <chigileychik_a@itstep.lan>
2022-08-31 21:46:24 +0300

    add login logic

--- src/components/ModalLogin.tsx
+++ src/components/ModalLogin.tsx
@@ -1,15 +1,15 @@
 import React, { useContext } from 'react';
 import Context from '../context/context';
-import Registration from './Registration';
+import Login from './Login';
 
-const Modal = () => {
-  const {openModal, setOpenModal} = useContext(Context);
+const ModalLogin = () => {
+  const { openModal, setOpenModal } = useContext(Context);
   return (
     <div className={`modal fade ${openModal && 'show d-block'}`} tabIndex={1} onClick={(event) => setOpenModal(false)}>
       <div className='modal-dialog' onClick={(event) => event.stopPropagation()}>
         <div className='modal modal-content'>
           <div className='modal-header'>
-            <h5 className='modal-title'>Modal title</h5>
+            <h5 className='modal-title'>Login or registration</h5>
             <button type='button'
                     className='btn-close'
                     data-bs-dismiss='modal'
@@ -18,13 +18,7 @@ const Modal = () => {
             </button>
           </div>
           <div className='modal-body'>
-            <Registration />
-          </div>
-          <div className='modal-footer'>
-            <button type='button' className='btn btn-secondary' data-bs-dismiss='modal'
-                    onClick={() => setOpenModal(false)}>Close
-            </button>
-            <button type='button' className='btn btn-primary'>Save changes</button>
+            <Login/>
           </div>
         </div>
         <div className={`modal-backdrop fade ${openModal && 'show d-block'}`}></div>
@@ -33,4 +27,4 @@ const Modal = () => {
   );
 };
 
-export default Modal;
+export default ModalLogin;
